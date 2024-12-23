36e235d1e540ec60179e184b4ea8673e3d06b9bc
JuleeC <juleshummel44@gmail.com>
2024-12-23 23:11:03 +0100

    private

---
+++ logical_operators.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class logical_operators {
+    public static void main(String[] args) {
+        int temp = 15;
+
+        if(temp>30) {
+            System.out.println("It is hot outside");
+        }
+        else if(temp>=20 && temp<=30) {
+            System.out.println("It is warm outside");
+        }
+        else {
+            System.out.println("It is cold outside");
+        }
+//EXAMPLE 2
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("playing game");
+        String response = scanner.next();
+
+        if(!response.equals("q") && !response.equals("Q")) {
+            System.out.println("still playing");
+        }
+        else {
+            System.out.println("You quit the game");
+        }
+
+    }
+}
