96076d7654392bfc3077b85c696b058a96388d2c
JuleeC <juleshummel44@gmail.com>
2024-12-25 23:57:48 +0100

    just learning ;)

--- final_keyword.java
+++ final_keyword.java
@@ -4,7 +4,7 @@ public class final_keyword {
         final double PI = 3.14159;
 
         //PI = 4;  //You can't change a final variable
-
+        //like const in python
         System.out.println(PI);
     }
 }
