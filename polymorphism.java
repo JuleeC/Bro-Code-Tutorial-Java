4b058568b99dc6b7e3b1697dbfccd17720def0ab
JuleeC <juleshummel44@gmail.com>
2025-01-05 14:10:12 +0100

    just learning :)

---
+++ polymorphism.java
@@ -0,0 +1,55 @@
+public class polymorphism {
+    public static void main(String[] args) {
+
+        // polymorphism = 	greek word for poly-"many", morph-"form"
+        //					The ability of an object to identify as more than one type
+
+        Car___ car = new Car___();
+        Bicycle bicycle = new Bicycle();
+        Boat boat = new Boat();
+
+        Vehicle_[] racers = {car,bicycle,boat};
+
+        for(Vehicle_ x : racers) {
+            x.go();
+        }
+
+    }
+}
+//**************************************
+class Vehicle_ {
+
+    public void go() {
+        // TODO Auto-generated method stub
+
+    }
+}
+//**************************************
+class Car___ extends Vehicle_{
+
+
+    @Override
+    public void go() {
+        System.out.println("*The car begins moving*");
+    }
+}
+//**************************************
+class Bicycle extends Vehicle_{
+
+
+    @Override
+    public void go() {
+        System.out.println("*The bicycle begins moving*");
+    }
+}
+//**************************************
+class Boat extends Vehicle_{
+
+
+    @Override
+    public void go() {
+        System.out.println("*The boat begins moving*");
+    }
+}
+//**************************************
+
