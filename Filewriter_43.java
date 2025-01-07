87abe094017c91d1cd00445736bb6b1456a90942
JuleeC <juleshummel44@gmail.com>
2025-01-07 18:54:34 +0100

    just learning :)

--- Filewriter_43.java
+++ Filewriter_43.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Filewriter_43 {
+
+    public static void main(String[] args) {
+
+        try {
+            FileWriter writer = new FileWriter("Java_learn/poem.txt");
+            writer.write("Roses are red \nViolets are blue \nBooty booty booty booty \nRockin' everywhere!");
+            writer.append("\n(A poem by Bro)");
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
