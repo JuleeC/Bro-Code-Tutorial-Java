79c29b22916ac7ed5ef7c8e72d3bcb64e99a8aa6
JuleeC <juleshummel44@gmail.com>
2025-01-10 18:01:59 +0100

    just learningg

--- GUIs/labels_47.java
+++ GUIs/labels_47.java
@@ -29,14 +29,14 @@ public class labels_47 {
         label.setIconTextGap(-25); //set gap of text to image
         label.setBackground(Color.black); //set background color
         label.setOpaque(true); //display background color
-        //label.setBorder(border); //sets border of label (not image+text)
+        label.setBorder(border); //sets border of label (not image+text)
         label.setVerticalAlignment(JLabel.CENTER); //set vertical position of icon+text within label
         label.setHorizontalAlignment(JLabel.CENTER); //set horizontal position of icon+text within label
         //label.setBounds(100, 100, 250, 250); //set x,y position within frame as well as dimensions
 
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.setSize(500,500);
+        frame.setSize(500,500);
         //frame.setLayout(null);
         frame.setVisible(true);
         frame.add(label);
