b0df57ef378d40bad2fb09b860059f36c4b6ef75
JuleeC <juleshummel44@gmail.com>
2025-01-15 21:32:57 +0100

    just learning :)

--- GUIs/opengui_54.java
+++ GUIs/opengui_54.java
@@ -38,30 +38,51 @@ class LaunchPage implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
 
+
         if(e.getSource()==myButton) {
+            System.out.println(myButton);
             frame.dispose();
             NewWindow myWindow = new NewWindow();
+
         }
     }
 }
 // ********************************************
 
-class NewWindow {
+class NewWindow implements ActionListener {
 
     JFrame frame = new JFrame();
     JLabel label = new JLabel("Hello!");
+    JButton myButton = new JButton("Go back!");
 
-    NewWindow(){
+    NewWindow() {
 
-        label.setBounds(0,0,100,50);
-        label.setFont(new Font(null,Font.PLAIN,25));
-
+        label.setBounds(0, 0, 100, 50);
+        label.setFont(new Font(null, Font.PLAIN, 25));
+        myButton.setBounds(100, 160, 200, 40);
+        myButton.setFocusable(false);
+        myButton.addActionListener(this);
         frame.add(label);
+        frame.add(myButton);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,420);
+        frame.setSize(420, 420);
         frame.setLayout(null);
         frame.setVisible(true);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+        if (e.getSource() == myButton) {
+            System.out.println(myButton);
+            frame.dispose();
+            LaunchPage launchpage = new LaunchPage();
+
+        }
     }
 }
 
