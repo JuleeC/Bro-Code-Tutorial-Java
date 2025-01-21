b10e7bb3e992eea472930fee85f013e03b0524a4
JuleeC <juleshummel44@gmail.com>
2025-01-21 17:31:52 +0100

    just learning :)

---
+++ colorchooser_64.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class colorchooser_64{
+
+    public static void main(String[] args) {
+
+        // JColorChooser = A GUI mechanism that let's a user choose a color
+
+        new MyyFrame();
+    }
+}
+
+
+public class MyyFrame extends JFrame implements ActionListener{
+
+
+    JButton button;
+    JLabel label;
+
+    MyyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Pick a color");
+        button.addActionListener(this);
+
+        label = new JLabel();
+        label.setBackground(Color.white);
+        label.setText("This is some text :D");
+        label.setFont(new Font("MV Boli",Font.PLAIN,100));
+        label.setOpaque(true);
+
+        this.add(button);
+        this.add(label);
+        this.pack();
+        this.setVisible(true);
+    }
+
+
+    }
+}
+
+
