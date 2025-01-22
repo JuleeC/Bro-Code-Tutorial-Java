e58f47b5f4809673644320397382c61b3e08eb53
JuleeC <juleshummel44@gmail.com>
2025-01-22 19:08:02 +0100

    just learning :)

--- GUIs/keylistener_65.java
+++ GUIs/keylistener_65.java
@@ -1,27 +1,46 @@
 package GUIs;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class keylistener_65 {
     public static void main(String[] args) {
-
+        new MMyFrame();
 
     }
 }
 class MMyFrame extends JFrame implements KeyListener {
+    JLabel label;
     MMyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLayout(null);
         this.addKeyListener(this);
-        this.setVisible(true);
 
+
+        label = new JLabel();
+        label.setBounds(0,0,100,100);
+        label.setBackground(Color.red);
+        label.setOpaque(true);
+        this.add(label);
+        this.setVisible(true);
     }
     @Override
     public void keyTyped(KeyEvent e){
         //invoked when a key is typed. Uses KeyChar, char output
+        switch(e.getKeyChar()) {
+            case 'a': label.setLocation(label.getX()-1,label.getY());
+                break;
+            case 'w': label.setLocation(label.getX(),label.getY()-1);
+                break;
+            case 's': label.setLocation(label.getX(),label.getY()+1);
+                break;
+            case 'd': label.setLocation(label.getX()+1,label.getY());
+                break;
+
+        }
     }
     @Override
     public void keyPressed(KeyEvent e){
@@ -31,6 +50,8 @@ class MMyFrame extends JFrame implements KeyListener {
     public void keyReleased(KeyEvent e){
         // called whenever a button is released
         System.out.println("You released key char: " + e.getKeyChar());
+        //with ascii diagramm
+        System.out.println("You released key code: " + e.getKeyCode());
     }
 
 
