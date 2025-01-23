24353c0217b374ecd177e89b002b4791b41d1b6b
JuleeC <juleshummel44@gmail.com>
2025-01-23 18:16:48 +0100

    just learning :)

---
+++ GUIs/mouselistener_66.java
@@ -0,0 +1,42 @@
+package GUIs;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class mouselistener_66 {
+
+}
+class MyFrraame extends JFrame implements MouseListener {
+    MyFrraame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLayout(null);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
