116245c3cf3fbc4fef21cbe23f68ad6c1a4b953f
JuleeC <juleshummel44@gmail.com>
2025-01-24 20:37:41 +0100

    just learning :)

--- GUIs/draganddrop_67.java
+++ GUIs/draganddrop_67.java
@@ -3,6 +3,7 @@ package GUIs;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 public class draganddrop_67 {
@@ -29,15 +30,24 @@ class DragPanel extends JPanel {
     Point imageCorner;
     Point prevPT;
     DragPanel() {
+        imageCorner = new Point(0,0);
+        ClickListener clickListener = new ClickListener();
+        DragListener dragListener = new DragListener();
+        this.addMouseListener(clickListener);
+        this.addMouseMotionListener(dragListener);
 
     }
 
     public void paintComponent(Graphics g) {
-
+        super.paintComponent(g);
+        image.paintIcon(this,g,(int)imageCorner.getX(),(int)imageCorner.getY());
     }
 
     private class ClickListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            prevPT = e.getPoint();
 
+        }
     }
 
     private class DragListener extends MouseMotionAdapter {
