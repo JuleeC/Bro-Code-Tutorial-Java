2bd8fc9ad796f912b7ae3dc5a9112ca76df45d97
JuleeC <juleshummel44@gmail.com>
2025-01-25 12:07:25 +0100

    Just learning :)

--- GUIs/keybinding_67.java
+++ GUIs/keybinding_67.java
@@ -1,6 +1,7 @@
 package GUIs;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class keybinding_67 {
@@ -8,9 +9,25 @@ Game game = new Game();
 }
 
 class Game {
-
+    JFrame frame;
+    JLabel label;
+    Action upAction;
+    Action downAction;
+    Action leftAction;
+    Action rightAction;
     Game() {
+        frame = new JFrame("Keybinding Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
 
+        label = new JLabel();
+        label.setBackground(Color.RED);
+        label.setBounds(100,100,100,100);
+        label.setOpaque(true);
+
+        frame.add(label);
     }
     public class UpAction extends AbstractAction {
 
