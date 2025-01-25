7e352749f0b5960d7e29e7db053e24839632d52b
JuleeC <juleshummel44@gmail.com>
2025-01-25 12:05:02 +0100

    Just learning :)

---
+++ GUIs/keybinding_67.java
@@ -0,0 +1,44 @@
+package GUIs;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class keybinding_67 {
+Game game = new Game();
+}
+
+class Game {
+
+    Game() {
+
+    }
+    public class UpAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    public class DownAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+
+    }
+    public class LeftAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+    public class RightAction extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
