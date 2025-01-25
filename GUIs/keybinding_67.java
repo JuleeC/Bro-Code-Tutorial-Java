141fa1e8ef700da791c3901583cb948974bfa9bc
JuleeC <juleshummel44@gmail.com>
2025-01-25 12:13:08 +0100

    Just learning :)

--- GUIs/keybinding_67.java
+++ GUIs/keybinding_67.java
@@ -9,6 +9,7 @@ Game game = new Game();
 }
 
 class Game {
+
     JFrame frame;
     JLabel label;
     Action upAction;
@@ -20,19 +21,39 @@ class Game {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
-        frame.setVisible(true);
+
 
         label = new JLabel();
         label.setBackground(Color.RED);
         label.setBounds(100,100,100,100);
         label.setOpaque(true);
 
+        upAction = new UpAction();
+        downAction = new DownAction();
+        leftAction = new LeftAction();
+        rightAction = new RightAction();
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("UP"),"upAction");
+        label.getActionMap().put("upAction",upAction);
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("DOWN"),"downAction");
+        label.getActionMap().put("downAction",downAction);
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("LEFT"),"leftAction");
+        label.getActionMap().put("leftAction",leftAction);
+
+        label.getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),"rightAction");
+        label.getActionMap().put("rightAction",rightAction);
+
+
         frame.add(label);
+        frame.setVisible(true);
     }
     public class UpAction extends AbstractAction {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            label.setLocation(label.getX(),label.getY()-10);
 
         }
     }
@@ -40,7 +61,7 @@ class Game {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            label.setLocation(label.getX(),label.getY()+10);
         }
 
     }
@@ -48,14 +69,14 @@ class Game {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            label.setLocation(label.getX()-10,label.getY());
         }
     }
     public class RightAction extends AbstractAction {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            label.setLocation(label.getX()+10,label.getY());
         }
     }
 }
