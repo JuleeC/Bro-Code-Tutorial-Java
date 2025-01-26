8b13327e038d859cc707b99f023ca3339a8df8bd
JuleeC <juleshummel44@gmail.com>
2025-01-26 20:15:55 +0100

    Just learning :)

--- GUIs/d2_graphics_68.java
+++ GUIs/d2_graphics_68.java
@@ -9,16 +9,31 @@ public class d2_graphics_68 {
     }
 }
 class MyFramme extends JFrame {
+    MyPanel panel;
     MyFramme() {
+        panel = new MyPanel();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(500,500);
         this.setLocationRelativeTo(null);
+        this.add(panel);
+        this.pack();
         this.setVisible(true);
+
 
     }
 
+
+}
+
+class MyPanel extends JPanel {
+
+    MyPanel() {
+        this.setPreferredSize(new Dimension(500,500));
+    }
     public void paint(Graphics g) {
         Graphics g2D = (Graphics) g;
         g2D.drawLine(0,0,500,500);
+
     }
+
 }
