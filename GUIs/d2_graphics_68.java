fd4e406df47d768e1f2ee7997a383c5fd24e716e
JuleeC <juleshummel44@gmail.com>
2025-01-26 20:25:03 +0100

    just learning :)

--- GUIs/d2_graphics_68.java
+++ GUIs/d2_graphics_68.java
@@ -31,9 +31,19 @@ class MyPanel extends JPanel {
         this.setPreferredSize(new Dimension(500,500));
     }
     public void paint(Graphics g) {
-        Graphics g2D = (Graphics) g;
-        g2D.drawLine(0,0,500,500);
-
+        Graphics g2D = (Graphics2D) g;
+        g2D.setColor(Color.blue);
+        //g2D.setStroke(new BasicStroke(5));
+        //g2D.drawLine(0,0,500,500);
+        //for all draw methods, see documents
+        g2D.drawRect(0,0,100,100);
+        int[] xPoints = {150,250,350};
+        int[] yPoints = {300,150,300};
+        g2D.setColor(Color.yellow);
+        g2D.drawPolygon(xPoints,yPoints,3);
+        g2D.setColor(Color.black);
+        g2D.setFont(new Font("Ink Free",Font.BOLD,50));
+        g2D.drawString("U R A WINNER !:)", 5,50);
     }
 
 }
