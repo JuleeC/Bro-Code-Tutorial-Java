93251aa90d393a307c0f6c8fc70f1c5f422365ed
JuleeC <juleshummel44@gmail.com>
2025-01-28 21:40:47 +0100

    just learning :)

--- GUIs/generic_classes_71.java
+++ GUIs/generic_classes_71.java
@@ -12,15 +12,43 @@ public class generic_classes_71 {
     }
 }
 class myIntegerClass {
+    Integer x;
 
+    myIntegerClass(Integer x) {
+        this.x = x;
+    }
+    public Integer getValue() {
+        return x;
+    }
 }
 
 class myDoubleClass  {
+    Double x;
 
+    myDoubleClass(Double x) {
+        this.x = x;
+    }
+    public Double getValue() {
+        return x;
+    }
 }
 class myCharacterClass {
+    Character x;
 
+    myCharacterClass(Character x) {
+        this.x = x;
+    }
+    public Character getValue() {
+        return x;
+    }
 }
 class myStringClass {
+    String x;
 
+    myStringClass(String x) {
+        this.x = x;
+    }
+    public String getValue() {
+        return x;
+    }
 }
