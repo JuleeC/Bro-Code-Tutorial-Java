8688943ae37ad1e5611d15d62748db1654199aad
JuleeC <juleshummel44@gmail.com>
2025-01-28 21:31:35 +0100

    just learning :)

--- GUIs/generics_70.java
+++ GUIs/generics_70.java
@@ -7,8 +7,12 @@ public class generics_70 {
         Double[] doubleArray = {5.5,4.4,3.3,2.2,1.1};
         Character[] charArray = {'H','E','L','L','O'};
         String[] stringArray = {"B","Y","E"};
+
+
         displayArray(intArray);
+        displayArray(doubleArray);
         displayArray(charArray);
+        displayArray(stringArray);
     }
     public static void displayArray(Integer[] array) {
         for(Integer x : array) {
@@ -16,4 +20,23 @@ public class generics_70 {
         }
         System.out.println();
     }
+    public static void displayArray(Double[] array) {
+        for(Double x : array) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+    public static void displayArray(Character[] array) {
+        for(Character x : array) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+    public static void displayArray(String[] array) {
+        for(String x : array) {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+    }
+
 }
