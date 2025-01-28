62b8b1405d69d18016305120ef9706c2f078d709
JuleeC <juleshummel44@gmail.com>
2025-01-28 21:44:58 +0100

    just learning :)

--- GUIs/generic_classes_71.java
+++ GUIs/generic_classes_71.java
@@ -3,52 +3,66 @@ package GUIs;
 public class generic_classes_71 {
     public static void main(String[] args) {
 
-        myIntegerClass myInt = new myIntegerClass(1);
-        myDoubleClass myDouble = new myDoubleClass(3.14);
-        myCharacterClass myChar = new myCharacterClass('@');
-        myStringClass myString = new myStringClass("Hello");
+        MyGenericClass<Integer> myInt = new MyGenericClass(1);
+        MyGenericClass<Double> myDouble = new MyGenericClass(3.14);
+        MyGenericClass<Character> myChar = new MyGenericClass('@');
+        MyGenericClass<String> myString = new MyGenericClass("Hello");
 
+        System.out.println(myInt.getValue());
+        System.out.println(myDouble.getValue());
+        System.out.println(myChar.getValue());
+        System.out.println(myString.getValue());
 
     }
 }
-class myIntegerClass {
-    Integer x;
 
-    myIntegerClass(Integer x) {
-        this.x = x;
+class MyGenericClass <Thing > {
+    Thing x;
+    MyGenericClass(Thing x) {
+        this.x=x;
     }
-    public Integer getValue() {
+    public Thing getValue() {
         return x;
     }
 }
-
-class myDoubleClass  {
-    Double x;
-
-    myDoubleClass(Double x) {
-        this.x = x;
-    }
-    public Double getValue() {
-        return x;
-    }
-}
-class myCharacterClass {
-    Character x;
-
-    myCharacterClass(Character x) {
-        this.x = x;
-    }
-    public Character getValue() {
-        return x;
-    }
-}
-class myStringClass {
-    String x;
-
-    myStringClass(String x) {
-        this.x = x;
-    }
-    public String getValue() {
-        return x;
-    }
-}
+//class myIntegerClass {
+//    Integer x;
+//
+//    myIntegerClass(Integer x) {
+//        this.x = x;
+//    }
+//    public Integer getValue() {
+//        return x;
+//    }
+//}
+//
+//class myDoubleClass  {
+//    Double x;
+//
+//    myDoubleClass(Double x) {
+//        this.x = x;
+//    }
+//    public Double getValue() {
+//        return x;
+//    }
+//}
+//class myCharacterClass {
+//    Character x;
+//
+//    myCharacterClass(Character x) {
+//        this.x = x;
+//    }
+//    public Character getValue() {
+//        return x;
+//    }
+//}
+//class myStringClass {
+//    String x;
+//
+//    myStringClass(String x) {
+//        this.x = x;
+//    }
+//    public String getValue() {
+//        return x;
+//    }
+//}
