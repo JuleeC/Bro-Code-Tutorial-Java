7db230885a5dbe29cef672cce9a5a94b1c4757a7
JuleeC <juleshummel44@gmail.com>
2025-01-28 21:38:53 +0100

    just learning :)

---
+++ GUIs/generic_classes_71.java
@@ -0,0 +1,26 @@
+package GUIs;
+
+public class generic_classes_71 {
+    public static void main(String[] args) {
+
+        myIntegerClass myInt = new myIntegerClass(1);
+        myDoubleClass myDouble = new myDoubleClass(3.14);
+        myCharacterClass myChar = new myCharacterClass('@');
+        myStringClass myString = new myStringClass("Hello");
+
+
+    }
+}
+class myIntegerClass {
+
+}
+
+class myDoubleClass  {
+
+}
+class myCharacterClass {
+
+}
+class myStringClass {
+
+}
