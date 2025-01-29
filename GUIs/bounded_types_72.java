a08689681c56978f1197688a2f73915e27ca6055
JuleeC <juleshummel44@gmail.com>
2025-01-29 18:34:33 +0100

    just learning :

--- GUIs/bounded_types_72.java
+++ GUIs/bounded_types_72.java
@@ -7,19 +7,20 @@ public class bounded_types_72 {
 
         MyGenericClasss<Integer,Integer> myInt = new MyGenericClasss(1,2);
         MyGenericClasss<Double,Double> myDouble = new MyGenericClasss(3.14,3.1);
+        //doesnt work because it isnt a subclass from the class Number
       //  MyGenericClasss<Character,Character> myChar = new MyGenericClasss('@','M');
       //  MyGenericClasss<String,String> myString = new MyGenericClasss("Hello","dsa");
 
         //ArrayList<String> myFriends = new ArrayList<>();
         HashMap<Integer,String> users = new HashMap<>();
 
-        System.out.println(myInt.getValue());
-        System.out.println(myDouble.getValue());
+        System.out.println(myInt.getValue_x());
+        System.out.println(myDouble.getValue_y());
 
 
     }
 }
-
+//only subclasses from the class Number can be the input. for example (Long,Integer,Float,Double)
 class MyGenericClasss <Thing extends Number,Thing2 extends Number > {
     Thing x;
     Thing y;
@@ -27,8 +28,11 @@ class MyGenericClasss <Thing extends Number,Thing2 extends Number > {
         this.x=x;
         this.y = y;
     }
-    public Thing getValue() {
+    public Thing getValue_x() {
         return x;
+    }
+    public Thing getValue_y() {
+        return y;
     }
 }
 
