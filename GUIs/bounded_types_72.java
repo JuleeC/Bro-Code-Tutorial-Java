31e0513ffd0c6d569e8919a6b16c2afaeb770fd5
JuleeC <juleshummel44@gmail.com>
2025-01-29 18:31:49 +0100

    just learning :

--- GUIs/bounded_types_72.java
+++ GUIs/bounded_types_72.java
@@ -7,21 +7,20 @@ public class bounded_types_72 {
 
         MyGenericClasss<Integer,Integer> myInt = new MyGenericClasss(1,2);
         MyGenericClasss<Double,Double> myDouble = new MyGenericClasss(3.14,3.1);
-        MyGenericClasss<Character,Character> myChar = new MyGenericClasss('@','M');
-        MyGenericClasss<String,String> myString = new MyGenericClasss("Hello","dsa");
+      //  MyGenericClasss<Character,Character> myChar = new MyGenericClasss('@','M');
+      //  MyGenericClasss<String,String> myString = new MyGenericClasss("Hello","dsa");
 
         //ArrayList<String> myFriends = new ArrayList<>();
         HashMap<Integer,String> users = new HashMap<>();
 
         System.out.println(myInt.getValue());
         System.out.println(myDouble.getValue());
-        System.out.println(myChar.getValue());
-        System.out.println(myString.getValue());
+
 
     }
 }
 
-class MyGenericClasss <Thing,Thing2 > {
+class MyGenericClasss <Thing extends Number,Thing2 extends Number > {
     Thing x;
     Thing y;
     MyGenericClasss(Thing x, Thing y) {
