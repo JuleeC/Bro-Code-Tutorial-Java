5d2138b4bca24239b959cd05f01deb2821ce4ea9
JuleeC <juleshummel44@gmail.com>
2025-01-30 21:24:55 +0100

    just learning:)

--- GUIs/serialization_73.java
+++ GUIs/serialization_73.java
@@ -1,5 +1,7 @@
 package GUIs;
 
+import java.io.*;
+
 public class serialization_73 {
     // a process that converts an objects into a byte stream
     //STEPS:
@@ -12,19 +14,26 @@ public class serialization_73 {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         User user = new User();
         user.name = "Bro";
         user.password = "Pizza";
 
-        user.sayHello();
+        FileOutputStream fileOut = new FileOutputStream("Java_learn//GUIs//UserInfo.ser");
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+
+        out.writeObject(user);
+        out.close();
+        fileOut.close();
+
+        System.out.println("object info saved :)");
 
     }
 
 }
 
-class User {
+class User implements Serializable {
     String name;
     String password;
 
