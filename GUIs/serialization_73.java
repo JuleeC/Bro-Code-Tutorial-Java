fd0615b60ed152cae7e6dd2544fbec7705f70a31
JuleeC <juleshummel44@gmail.com>
2025-01-30 21:16:07 +0100

    just learning:)

---
+++ GUIs/serialization_73.java
@@ -0,0 +1,17 @@
+package GUIs;
+
+public class serialization_73 {
+    // a process that converts an objects into a byte stream
+    public static void main(String[] args) {
+
+    }
+}
+
+class User {
+    String name;
+    String password;
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
