bbfc91131900d63f2a706e9ee923f82f7a550ebe
JuleeC <juleshummel44@gmail.com>
2025-02-01 19:20:44 +0100

    just learning :)

--- GUIs/timertask_76.java
+++ GUIs/timertask_76.java
@@ -1,5 +1,6 @@
 package GUIs;
 
+import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,7 +13,16 @@ public class timertask_76 {
                 System.out.println("Task is complete ");
             }
         };
+        Calendar date = Calendar.getInstance();
+        date.set(Calendar.YEAR,2025);
+        date.set(Calendar.MONTH,Calendar.FEBRUARY);
+        date.set(Calendar.DAY_OF_MONTH,1);
+        date.set(Calendar.HOUR_OF_DAY,0);
+        date.set(Calendar.MINUTE,0);
+        date.set(Calendar.SECOND,0);
 
-
+        //timer.schedule(task,date.getTime());
+        //timer.schedule(task,0);
+        timer.scheduleAtFixedRate(task,0,1000);
     }
 }
