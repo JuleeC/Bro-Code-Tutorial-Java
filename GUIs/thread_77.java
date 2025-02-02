0a47c1ee826c39aa8b34e0be6bf1b6dbd0e5ee3f
JuleeC <juleshummel44@gmail.com>
2025-02-02 16:16:41 +0100

    just learning :)

--- GUIs/thread_77.java
+++ GUIs/thread_77.java
@@ -1,11 +1,23 @@
 package GUIs;
 
 public class thread_77 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //thread = execution in a programm.
         // the JVM allows an application to have multiple threads running concurrent.
         // each threads executes parts of your code in parallel with the main thread.
         // the Java Virtual Machine continues to execute threads until the exit method of class Runtime has been called or all user threads have died.
+
+        //System.out.println(Thread.activeCount());
+        //Thread.currentThread().setName("MAINNNN");
+        //System.out.println(Thread.currentThread().getName());
+        //Thread.currentThread().setPriority(10);
+        //System.out.println(Thread.currentThread().getPriority());
+        System.out.println(Thread.currentThread().isAlive());
+
+        for(int i = 3; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
     }
 }
 
