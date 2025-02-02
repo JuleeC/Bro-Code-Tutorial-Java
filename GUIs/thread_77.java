eadc61c9be149f2699d5299a95dd642ada2267a6
JuleeC <juleshummel44@gmail.com>
2025-02-02 16:11:07 +0100

    just learning :)

---
+++ GUIs/thread_77.java
@@ -0,0 +1,11 @@
+package GUIs;
+
+public class thread_77 {
+    public static void main(String[] args) {
+        //thread = execution in a programm.
+        // the JVM allows an application to have multiple threads running concurrent.
+        // each threads executes parts of your code in parallel with the main thread.
+        // the Java Virtual Machine continues to execute threads until the exit method of class Runtime has been called or all user threads have died.
+    }
+}
+
