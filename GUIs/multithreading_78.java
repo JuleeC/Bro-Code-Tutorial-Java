0b9afd8d417804f1acb816454aefeccd40ffc99f
JuleeC <juleshummel44@gmail.com>
2025-02-03 20:42:35 +0100

    Just learning :)

---
+++ GUIs/multithreading_78.java
@@ -0,0 +1,7 @@
+package GUIs;
+
+public class multithreading_78 {
+    //multithreading = Process of executing multiple threads simulataneously
+    //                 Helps maximum utliziation of CPU
+    //                 Threads are independent, they dont affect the execution of other threads
+}
