56ff90a8a255e22d3673ed061d5d29cc6550e825
JuleeC <juleshummel44@gmail.com>
2025-02-03 20:53:42 +0100

    Just learning :)

--- GUIs/multithreading_78.java
+++ GUIs/multithreading_78.java
@@ -5,18 +5,28 @@ public class multithreading_78 {
     //                 Helps maximum utliziation of CPU
     //                 Threads are independent, they dont affect the execution of other threads
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
+        //create sublass of Thread
         MyThreadss thread1 = new MyThreadss();
+        //or
 
+        // implement Runnable interface and pass instance as an argument to Threads
         MyRunnable runnable1 = new MyRunnable();
         Thread thread2 = new Thread(runnable1);
+
+        thread1.setDaemon(true);
+        thread2.setDaemon(true);
+
+        thread1.start();
+        thread1.join(3000); // calling thread wait until the specified thread dies  or for x milliseconds
+        thread2.start();
     }
 }
 class MyThreadss extends Thread {
     @Override
     public void run() {
         for(int i = 10; i >0; i--){
-            System.out.println("Thread #1 : " +1 );
+            System.out.println("Thread #1 : " +i );
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -30,6 +40,14 @@ class MyThreadss extends Thread {
 class MyRunnable implements Runnable {
     @Override
     public void run() {
-
+        for(int i = 0; i <10; i++){
+            System.out.println("Thread #2 : " +i );
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Thread1 is finished");
     }
 }
