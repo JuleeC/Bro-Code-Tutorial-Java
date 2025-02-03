8fc0bee4cfa8e51bb47f227edb512e16bb38d7c8
JuleeC <juleshummel44@gmail.com>
2025-02-03 20:44:55 +0100

    Just learning :)

--- GUIs/multithreading_78.java
+++ GUIs/multithreading_78.java
@@ -4,4 +4,16 @@ public class multithreading_78 {
     //multithreading = Process of executing multiple threads simulataneously
     //                 Helps maximum utliziation of CPU
     //                 Threads are independent, they dont affect the execution of other threads
+
+    public static void main(String[] args){
+        MyThreadss thread1 = new MyThreadss();
+    }
+}
+class MyThreadss extends Thread {
+    @Override
+    public void run() {
+        for(int i = 10; i >0; i--){
+            System.out.println("Thread #1 : " +1 );
+        }
+    }
 }
