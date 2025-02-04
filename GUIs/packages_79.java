e3a279fb6936a18e266678889b62f4b0ad270f5a
JuleeC <juleshummel44@gmail.com>
2025-02-04 16:38:44 +0100

    Just learning :)

--- GUIs/packages_79.java
+++ GUIs/packages_79.java
@@ -2,5 +2,6 @@ package GUIs;
 
 public class packages_79 {
     // a package is like a folder where your classes are stored
-
+    // if you want to do an instance of a class that isnt located in the package where you want to do an instance of, you need to import it
+    // example : import javax.swing.Icon;
 }
