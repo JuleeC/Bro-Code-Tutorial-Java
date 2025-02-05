34709499c08592c71a2723bea91c686d5e5cff6f
JuleeC <juleshummel44@gmail.com>
2025-02-05 22:07:51 +0100

    just learning :)

--- GUIs/compile_and_run_80.java
+++ GUIs/compile_and_run_80.java
@@ -6,4 +6,6 @@ public class compile_and_run_80 {
     // open command prompt (windows) or Terminal (Mac) on your computer
     // set path=C:\Program Files\Java\jdk-13.0.1\bin (where JDK is located)
     // cd C:\Users\Cakow\Desktop (or whereever you java file is)
+    // javac HelloWorld.java (to compile)
+    // java Helloworld (to run a .class file, its protable)
 }
