d841e1ef08d3c12a03aa7eb48689ff873a9dcdd6
JuleeC <juleshummel44@gmail.com>
2025-02-05 22:07:19 +0100

    just learning :)

--- GUIs/compile_and_run_80.java
+++ GUIs/compile_and_run_80.java
@@ -3,4 +3,7 @@ package GUIs;
 public class compile_and_run_80 {
     // make sure you have a JDK installed
     // (optional) use a text editor and save a file as .java
+    // open command prompt (windows) or Terminal (Mac) on your computer
+    // set path=C:\Program Files\Java\jdk-13.0.1\bin (where JDK is located)
+    // cd C:\Users\Cakow\Desktop (or whereever you java file is)
 }
