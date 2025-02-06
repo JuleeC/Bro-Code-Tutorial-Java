f757a59b04b1b96a9544f5e479389c7b3b32d0a5
JuleeC <juleshummel44@gmail.com>
2025-02-06 22:05:01 +0100

    Just learning :)

---
+++ GUIs/executable_81.java
@@ -0,0 +1,13 @@
+package GUIs;
+
+public class executable_81 {
+    //on Eclipse
+    // Right click on Java Project (pick a project with a GUI)
+    //Export
+    //Java > Runnable Jar file
+    //at launch configuration select your project
+    //at export destination, select where you want this jar file exported to
+    //finished
+
+
+}
