c2628c4c15c1bfd269cfeccd71ff7a1d1efa79f4
JuleeC <juleshummel44@gmail.com>
2025-02-09 17:49:56 +0100

    just learning :)

--- algorithms_82/stacks_84.java
+++ algorithms_82/stacks_84.java
@@ -26,6 +26,7 @@ public class stacks_84 {
 //        stack.pop();
 //        stack.pop();
 //        stack.pop();
+
         //exception if stack is empty but the pop method is called
 
         //String myFavGame = stack.pop();
@@ -33,6 +34,21 @@ public class stacks_84 {
         //System.out.println(stack.peek());
 
 
-        System.out.println(stack);
+        System.out.println(stack.search("DOOM"));
+        //if youre searching for an item that isnt in the stack, -1 will be the output
+        System.out.println(stack.search("asddsa"));
+
+        //exception because the memory is out of heap
+//        for(int i = 0; i < i+1; i++) {
+//            stack.push("Fallout76");
+//        }
+
+        //uses of stacks?
+        //1. undo/redo features in text editors
+        //2. moving back/forward through browser history
+        //3. backtracking algorithms (maze, file directories)
+        //4. calling functions (call stack)
+
+
     }
 }
