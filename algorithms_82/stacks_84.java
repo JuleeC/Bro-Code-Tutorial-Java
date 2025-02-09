81165ffbc86fda14bff665d711da8ce332f4c611
JuleeC <juleshummel44@gmail.com>
2025-02-09 17:37:08 +0100

    just learning :)

---
+++ algorithms_82/stacks_84.java
@@ -0,0 +1,10 @@
+package algorithms_82;
+
+public class stacks_84 {
+    //stack = LIFO data structure. Last-In First-Out
+    //        stores objects into a sort of "vertical tower"
+    //        push() to add to the top
+    //        pop() to remove from the top
+
+
+}
