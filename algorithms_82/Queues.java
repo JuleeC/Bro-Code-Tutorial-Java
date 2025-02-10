4ef98005c244868ea780a5bc9f444fcb7c98b840
JuleeC <juleshummel44@gmail.com>
2025-02-10 21:12:20 +0100

    Just learning :)

---
+++ algorithms_82/Queues.java
@@ -0,0 +1,10 @@
+package algorithms_82;
+
+public class Queues {
+//    Queue = FIFO data structure, First-IN First-OUT ( ex. A line of people)
+//            A collection designed for holding elements prior to processing
+//            Linear data structure
+//
+//            add = enqueue, offer()
+//            remove = dequeue, poll()
+}
