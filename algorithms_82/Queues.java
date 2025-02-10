85b4c51858d901fd31b40c1bd38e8052e1072584
JuleeC <juleshummel44@gmail.com>
2025-02-10 21:16:43 +0100

    Just learning :)

--- algorithms_82/Queues.java
+++ algorithms_82/Queues.java
@@ -1,5 +1,8 @@
 package algorithms_82;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Queues {
 //    Queue = FIFO data structure, First-IN First-OUT ( ex. A line of people)
 //            A collection designed for holding elements prior to processing
@@ -7,4 +10,15 @@ public class Queues {
 //
 //            add = enqueue, offer()
 //            remove = dequeue, poll()
+    public static void main(String[] args) {
+
+        Queue<String> queue = new LinkedList<String>();
+
+        queue.offer("karen");
+        queue.offer("chad");
+        queue.offer("steve");
+        queue.offer("harold");
+
+        System.out.println(queue);
+    }
 }
