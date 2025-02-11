246b394362cab287da47a7f9ce485255d931c758
JuleeC <juleshummel44@gmail.com>
2025-02-11 21:13:36 +0100

    just learning :)

---
+++ algorithms_82/priorityqueues_85.java
@@ -0,0 +1,15 @@
+package algorithms_82;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class priorityqueues_85 {
+    //Priority Queue = a FIFO data strucutre that serves elements
+    //                 with the highest priorities first
+    //                 before elements with lower priority.
+
+    Queue<Double> queuee = new LinkedList<>();
+
+    queuee.offer(1.0);
+
+}
