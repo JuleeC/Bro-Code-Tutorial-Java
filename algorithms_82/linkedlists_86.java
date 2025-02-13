7f2fef5c52fd7f14a0d34865ec52d12d6f60ec93
JuleeC <juleshummel44@gmail.com>
2025-02-13 17:23:22 +0100

    Just learning :)

--- algorithms_82/linkedlists_86.java
+++ algorithms_82/linkedlists_86.java
@@ -24,8 +24,16 @@ public class linkedlists_86 {
 //    1. Dynamic Storage (allocations needed memory while running)
 //    2. Insertion and Deletion of Nodes is easy (I/O)
 //    3. No/Low memory waste
-
-
+//
+//    disadvantages?
+//    1. Greater memory usage (additional pointer)
+//    2. No random acces of elements (no index [i]
+//    3. Accessing/searching elements is more time consuming. O(n)
+//
+//    uses?
+//    1. implements Stacks/Queues
+//    2. GPS navigation
+//    3. music playlist
 
     public static void main(String[] args) {
 
