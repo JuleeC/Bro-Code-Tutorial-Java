429d53c8be44e3b7ad373438396e108e10a54018
JuleeC <juleshummel44@gmail.com>
2025-02-13 17:11:32 +0100

    Just learning :)

---
+++ algorithms_82/linkedlists_86.java
@@ -0,0 +1,15 @@
+package algorithms_82;
+
+import java.util.LinkedList;
+
+public class linkedlists_86 {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> linkedlist = new LinkedList<String>();
+
+        linkedlist.push("A");
+
+        System.out.println(linkedlist);
+    }
+}
