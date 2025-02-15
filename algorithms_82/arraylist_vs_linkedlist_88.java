ffd70822f70b7bc399e87fa407143d77dc243625
JuleeC <juleshummel44@gmail.com>
2025-02-15 17:59:46 +0100

    just learning :)

---
+++ algorithms_82/arraylist_vs_linkedlist_88.java
@@ -0,0 +1,16 @@
+package algorithms_82;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class arraylist_vs_linkedlist_88 {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+    }
+}
