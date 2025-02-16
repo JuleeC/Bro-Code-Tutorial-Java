7298a318f2f5f5672695851c738f00f7421cc776
JuleeC <juleshummel44@gmail.com>
2025-02-16 21:49:14 +0100

    just learning :)

---
+++ basics_big_o_notation_89.java
@@ -0,0 +1,14 @@
+public class basics_big_o_notation_89 {
+
+//    "How code slows as data grow"
+//
+//    1. Describes the perfomance of an algorithm as the amount of data increases
+//    2. Machine independent (# of steps to completion)
+//    3. Ignore smaller operations O(n + 1) -> O(n)
+
+//    example:        n = amount of data
+//    O(1)        (its a variable like x)
+//    O(n)
+//    O(log n)
+//    O(n*n)
+}
