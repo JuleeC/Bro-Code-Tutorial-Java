0048ab7b911e103a8a5c4d52e5342c24e710a0ea
JuleeC <juleshummel44@gmail.com>
2025-02-17 21:29:19 +0100

    just learning :)

---
+++ algorithms_82/linear_search_90.java
@@ -0,0 +1,16 @@
+package algorithms_82;
+
+public class linear_search_90 {
+    //linear search = iterate through a collection one element at a time
+
+    // runtime complexity: O(n)
+
+    //Disadvantages:
+    //Slow for large data sets
+
+    //Advantages:
+    //Fast for searches of small to medium data sets
+    //Does not need to sorted
+    //Useful for data structures that do not have random access (Linked List)
+
+}
