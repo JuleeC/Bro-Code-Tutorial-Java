29b6f12ad33197f6077f98fa4dcf646ace0a0a9c
JuleeC <juleshummel44@gmail.com>
2025-02-17 21:30:50 +0100

    just learning :)

--- algorithms_82/linear_search_90.java
+++ algorithms_82/linear_search_90.java
@@ -12,5 +12,14 @@ public class linear_search_90 {
     //Fast for searches of small to medium data sets
     //Does not need to sorted
     //Useful for data structures that do not have random access (Linked List)
+    public static void main(String[] args) {
+        int[] array = {9,1,8,2,7,3,6,4,5};
+
+        int index = linearSearch(array,1);
+
+    }
+    private static int linearSearch(int[] array, int value) {
+        return 0;
+    }
 
 }
