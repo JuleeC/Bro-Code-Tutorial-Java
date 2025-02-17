8714918aef8e64c3ddd77e24e4b4b94c510adafc
JuleeC <juleshummel44@gmail.com>
2025-02-17 21:32:49 +0100

    just learning :)

--- algorithms_82/linear_search_90.java
+++ algorithms_82/linear_search_90.java
@@ -17,6 +17,12 @@ public class linear_search_90 {
 
         int index = linearSearch(array,1);
 
+        if(index != -1 ) {
+            System.out.println("Element found at index " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+
     }
     private static int linearSearch(int[] array, int value) {
         for(int i = 0; i < array.length; i++) {
