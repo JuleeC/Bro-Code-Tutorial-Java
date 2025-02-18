739a42cdefd9e3d9d9102d1beb3977af3bdf884e
JuleeC <juleshummel44@gmail.com>
2025-02-18 21:18:11 +0100

    Just learning :)

--- algorithms_82/binary_search_91.java
+++ algorithms_82/binary_search_91.java
@@ -16,5 +16,22 @@ public class binary_search_91 {
 
         int index = Arrays.binarySearch(array,target);
 
+        if(index == -1 ) {
+            System.out.println(target +  " not found");
+
+        }else{
+            System.out.println("Element found at: " + index);
+        }
+    }
+    public static int binarySearch(int[] array, int target) {
+        int low = 0;
+        int high = array.length-1;
+        while(low <= high) {
+            int middle = low + (high -low) / 2;
+            int value = array[middle];
+            System.out.println("middle: " + value);
+
+        }
+        return -1;
     }
 }
