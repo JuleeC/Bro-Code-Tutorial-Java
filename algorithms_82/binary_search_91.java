d659ea0c029ed53fd09cc89afeb4ae14d2ca72c1
JuleeC <juleshummel44@gmail.com>
2025-02-18 21:15:36 +0100

    Just learning :)

--- algorithms_82/binary_search_91.java
+++ algorithms_82/binary_search_91.java
@@ -1,11 +1,20 @@
 package algorithms_82;
 
+import java.util.Arrays;
+
 public class binary_search_91 {
     // binary search = algorithm that finds the position of a target value within a sorted array.
     //                 Half of the array is eliminated during each "step"
 
     public static void main(String[] args) {
+        int array[] = new int[100];
+        int target = 42;
 
+        for(int i = 0; i< array.length;i++) {
+            array[i] = i;
+        }
+
+        int index = Arrays.binarySearch(array,target);
 
     }
 }
