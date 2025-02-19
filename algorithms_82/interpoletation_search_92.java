4e38265575304aff94df724c56dd163321bd4355
JuleeC <juleshummel44@gmail.com>
2025-02-19 18:03:46 +0100

    just learning :)

---
+++ algorithms_82/interpoletation_search_92.java
@@ -0,0 +1,10 @@
+package algorithms_82;
+
+public class interpoletation_search_92 {
+    // interpolation search = improvement over binary search best used for "uniformly" distributed data
+    //                        "guesses" where a value might be based on calculated probe results
+    //                        if probe is incorrect, search area is narrowed, and a new probe is calculated
+
+    //                        average case: O(log(log(n)))
+    //                        worst case: O(n) [values increase exponentially]
+}
