8cd374a9d803007fa91275b81f611931339b3329
JuleeC <juleshummel44@gmail.com>
2025-02-19 18:15:06 +0100

    just learning :)

--- algorithms_82/interpoletation_search_92.java
+++ algorithms_82/interpoletation_search_92.java
@@ -9,8 +9,8 @@ public class interpoletation_search_92 {
     //                        worst case: O(n) [values increase exponentially]
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int index = interpolationSearch(array, 8);
+        int[] array = {1, 2,4,8,16,32,64,128,256,512,1024,2048};
+        int index = interpolationSearch(array, 33);
         if(index != -1) {
             System.out.println("Element found at index: " + index);
         }else{
@@ -22,16 +22,18 @@ public class interpoletation_search_92 {
         int high = array.length - 1;
         int low = 0;
         while (value >= array[low] && value <= array[high] && low <= high) {
-            int probe = (high - low) * (value - array[low]) / (array[high] - array[low]);
+            int probe = low + (high - low) * (value - array[low]) / (array[high] - array[low]);
 
             System.out.println("probe:" + probe);
-            if (array[probe] == value) return probe;
+            if (array[probe] == value){
+                return probe;
+            }
             else if (array[probe] < value) {
                 low = probe + 1;
             } else {
 
             }
-            return -1;
+
         }
         return -1;
     }
