97d1d48f6d6eef9ee3d6a338fa73a1481a7b7616
JuleeC <juleshummel44@gmail.com>
2025-02-19 18:06:13 +0100

    just learning :)

--- algorithms_82/interpoletation_search_92.java
+++ algorithms_82/interpoletation_search_92.java
@@ -7,4 +7,19 @@ public class interpoletation_search_92 {
 
     //                        average case: O(log(log(n)))
     //                        worst case: O(n) [values increase exponentially]
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6,7,8,9};
+         int index = interpolationSearch(array,8);
+    }
+    private static int interpolationSearch(int[] array, int value) {
+        int high = array.length - 1;
+        int low = 0;
+        while(value >= array[low] && value <= array[high] && low <= high) {
+
+        }
+        return 0;
+    }
+
 }
+
