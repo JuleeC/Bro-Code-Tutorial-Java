00018fdbc22606178e4656fc723b786b321ff4a2
JuleeC <juleshummel44@gmail.com>
2025-02-20 17:05:58 +0100

    Just learning :)

--- algorithms_82/bubble_sort_93.java
+++ algorithms_82/bubble_sort_93.java
@@ -18,8 +18,14 @@ public class bubble_sort_93 {
 
     }
     public static void bubbleSort(int array[] ) {
-        for() {
-
+        for(int i = 0; i< array.length-1; i++) {
+            for(int j = 0; j< array.length-i-1; j++) {
+                if(array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j +1];
+                    array[j+1] = temp;
+                }
+            }
         }
 
     }
