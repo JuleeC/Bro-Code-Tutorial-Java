8b7973d8dd89622e0f04867f0a90bb4787fd06b6
JuleeC <juleshummel44@gmail.com>
2025-02-20 17:03:30 +0100

    Just learning :)

--- algorithms_82/bubble_sort_93.java
+++ algorithms_82/bubble_sort_93.java
@@ -3,8 +3,13 @@ package algorithms_82;
 public class bubble_sort_93 {
     // bubble sort = pars of adjacent elements are compared , and the elements
     //               swapped if they are not in order.
-
+    //
+    //               Quadratic time O(n*n)
+    //               small data set = okay-ish
+    //               large data set = bad
     public static void main(String[] args) {
+        int[] array ={0,1,6,7,4,32,7,8,5,9,2};
+
 
 
     }
