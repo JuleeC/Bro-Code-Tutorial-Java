e60947693c38e7380fe9252a6e432e0701432c2d
JuleeC <juleshummel44@gmail.com>
2025-02-22 08:22:16 +0100

    just learning :)

---
+++ algorithms_82/selection_sort_94.java
@@ -0,0 +1,11 @@
+package algorithms_82;
+
+public class selection_sort_94 {
+    //selection sort = search through an array and keep track of the minimum value during
+    //                 each iteration. At the end of each iteration, we swap variables
+    public static void main(String[] args) {
+
+
+
+    }
+}
