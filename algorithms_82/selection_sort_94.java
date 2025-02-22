d022c25ff6b6af6b2aa3e53dc29f00dcf2363131
JuleeC <juleshummel44@gmail.com>
2025-02-22 08:25:33 +0100

    just learning :)

--- algorithms_82/selection_sort_94.java
+++ algorithms_82/selection_sort_94.java
@@ -8,8 +8,27 @@ public class selection_sort_94 {
     //small data set = okay
     //large data set = BAD
     public static void main(String[] args) {
+        int array[] = {8,7,9,2,3,5,1,6,4};
+
+        selectionSort(array);
+
+        for(int i : array) {
+            System.out.println(array);
+        }
 
 
+    }
+    public static void selectionSort(int[] array) {
+        for(int i = 0; i< array.length-1; i++) {
+            int min = i;
+            for(int j = 0; j< array.length; j++) {
+                if(array[min] > array[j]) {
+                    min = j;
+                }
+            }
+            int temp = array[i];
+
+        }
 
     }
 }
