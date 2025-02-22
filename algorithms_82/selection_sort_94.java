a1cb4bdf5ab4d6e1d5282dcab97eeb28418fe24c
JuleeC <juleshummel44@gmail.com>
2025-02-22 08:22:52 +0100

    just learning :)

--- algorithms_82/selection_sort_94.java
+++ algorithms_82/selection_sort_94.java
@@ -3,6 +3,10 @@ package algorithms_82;
 public class selection_sort_94 {
     //selection sort = search through an array and keep track of the minimum value during
     //                 each iteration. At the end of each iteration, we swap variables
+
+    //Qudratic time O(n*n)
+    //small data set = okay
+    //large data set = BAD
     public static void main(String[] args) {
 
 
