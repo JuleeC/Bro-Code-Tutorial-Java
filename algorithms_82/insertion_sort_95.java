a5618dfa322bdfce5e151a4b0ec14f0fe53b4959
JuleeC <juleshummel44@gmail.com>
2025-02-23 21:01:08 +0100

    Just learning :)

---
+++ algorithms_82/insertion_sort_95.java
@@ -0,0 +1,12 @@
+package algorithms_82;
+
+public class insertion_sort_95 {
+
+    //insertion sort = after comparing elements to the left
+    //                 shift elements to the right ot make room to insert value
+
+    public static void main (String[] args) {
+
+
+    }
+}
