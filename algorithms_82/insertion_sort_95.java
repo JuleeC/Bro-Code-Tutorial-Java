d4c3f481d7ac0a98d1ce8f48abb19c0c8da0b598
JuleeC <juleshummel44@gmail.com>
2025-02-23 21:04:03 +0100

    Just learning :)

--- algorithms_82/insertion_sort_95.java
+++ algorithms_82/insertion_sort_95.java
@@ -4,7 +4,13 @@ public class insertion_sort_95 {
 
     //insertion sort = after comparing elements to the left
     //                 shift elements to the right ot make room to insert value
+    //
+    //Quadratic time O(n*n)
+    //small data set = decent
+    //large data set = BAD
 
+    //Less steps than bubble sort
+    //best case is O(n) compared to Selection Sort O(n*n)
     public static void main (String[] args) {
 
 
