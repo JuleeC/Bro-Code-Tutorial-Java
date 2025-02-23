a2b1e0b93241c4200ab2f60dd0b38b702d33d555
JuleeC <juleshummel44@gmail.com>
2025-02-23 21:08:04 +0100

    Just learning :)

--- algorithms_82/insertion_sort_95.java
+++ algorithms_82/insertion_sort_95.java
@@ -25,8 +25,11 @@ public class insertion_sort_95 {
             int temp = array[i];
             int j = i-1;
 
-            whil
-
+            while(j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j +1] = temp;
         }
 
     }
