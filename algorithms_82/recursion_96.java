20db36a53c14e4a4ee255e061267f23b16e4ebd3
JuleeC <juleshummel44@gmail.com>
2025-02-24 19:28:57 +0100

    Just learning :

---
+++ algorithms_82/recursion_96.java
@@ -0,0 +1,8 @@
+package algorithms_82;
+
+public class recursion_96 {
+    //recursion = when a thing is defined in terms of itself.
+    //            Apply the result of a procedure, to a procedure
+    //            A recursive method calls itself. Can be a substitute for iteration.
+    //
+}
