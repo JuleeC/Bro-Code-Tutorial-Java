6f949fd2b2be945b1f241eca5e6e737362936ed5
JuleeC <juleshummel44@gmail.com>
2025-02-24 19:32:16 +0100

    Just learning :

--- algorithms_82/recursion_96.java
+++ algorithms_82/recursion_96.java
@@ -4,5 +4,16 @@ public class recursion_96 {
     //recursion = when a thing is defined in terms of itself.
     //            Apply the result of a procedure, to a procedure
     //            A recursive method calls itself. Can be a substitute for iteration.
+    //            Divide a problem into sub-problems of the same type as the original
+    //            Commonly used with advanced sorting algorithms and navigating trees
     //
+     //Advantages:
+    //easier to read,write
+    //easier to debug
+
+    //Disadvantages:
+    //sometimes slower
+    //uses more memory
+
+
 }
