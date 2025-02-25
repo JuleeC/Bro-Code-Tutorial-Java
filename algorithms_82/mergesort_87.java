cdbf1b1327e7d207b63c53f96b3a4e194dfbf6f1
JuleeC <juleshummel44@gmail.com>
2025-02-25 18:31:40 +0100

    Just learning :)

---
+++ algorithms_82/mergesort_87.java
@@ -0,0 +1,20 @@
+package algorithms_82;
+
+public class mergesort_87 {
+
+
+    public static void main(String[] args) {
+        int[] array = {8,2,3,6,4,5,9,1,7};
+
+        mergeSort(array);
+
+        for(int i : array) {
+            System.out.print(i);
+        }
+
+    }
+    public static void mergeSort(int[] array) {
+
+
+    }
+}
