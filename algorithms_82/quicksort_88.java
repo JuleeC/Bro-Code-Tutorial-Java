3304af7ecd8434c0c0f31d794337b75181fca8cd
JuleeC <juleshummel44@gmail.com>
2025-02-26 17:52:35 +0100

    just learning :)

--- algorithms_82/quicksort_88.java
+++ algorithms_82/quicksort_88.java
@@ -4,10 +4,16 @@ public class quicksort_88 {
     public static void main(String[] args) {
         int[] array = {1,5,7,9,2,4,3,8,6};
 
+        quickSort(array,0,array.length-1);
 
         for(int i : array) {
             System.out.print(i);
         }
+
+    }
+
+    private static void quickSort(int[] array, int start, int end) {
+        if(end <= start) return; //base case
 
     }
 }
