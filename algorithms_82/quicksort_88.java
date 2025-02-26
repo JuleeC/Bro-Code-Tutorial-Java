fabbdd5803de5dc1e395129a9881c3bc571515ae
JuleeC <juleshummel44@gmail.com>
2025-02-26 17:55:56 +0100

    just learning :)

--- algorithms_82/quicksort_88.java
+++ algorithms_82/quicksort_88.java
@@ -23,6 +23,14 @@ public class quicksort_88 {
         int pivot = array[end];
         int i =start -1;
 
+        for(int j = start; j <= end -1 ; j++) {
+            if(array[j] < pivot) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
         return i;
     }
 
