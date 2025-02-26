f805a6be15f1d52e627ded38042c51a21a90870a
JuleeC <juleshummel44@gmail.com>
2025-02-26 17:51:39 +0100

    just learning :)

--- algorithms_82/quicksort_88.java
+++ algorithms_82/quicksort_88.java
@@ -1,4 +1,13 @@
 package algorithms_82;
 
 public class quicksort_88 {
+    public static void main(String[] args) {
+        int[] array = {1,5,7,9,2,4,3,8,6};
+
+
+        for(int i : array) {
+            System.out.print(i);
+        }
+
+    }
 }
