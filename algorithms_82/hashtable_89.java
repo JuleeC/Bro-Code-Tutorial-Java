4167fbd56305bae03740517d0c52b957c065e74c
JuleeC <juleshummel44@gmail.com>
2025-02-27 18:17:57 +0100

    just learning :)

---
+++ algorithms_82/hashtable_89.java
@@ -0,0 +1,12 @@
+package algorithms_82;
+
+import java.util.Hashtable;
+
+public class hashtable_89 {
+    public static void main(String[] args) {
+        Hashtable<Integer,String> table = new Hashtable<>(10);
+
+        table.put(100,"Spongebob");
+
+    }
+}
