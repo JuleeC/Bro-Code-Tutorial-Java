18266e113e115de5715619de8efb9796d4023ed4
JuleeC <juleshummel44@gmail.com>
2025-03-02 21:49:25 +0100

    Just learning :)

--- algorithms_82/adjacency_matrix_96.java
+++ algorithms_82/adjacency_matrix_96.java
@@ -21,6 +21,20 @@ class Graph {
     Graph(int size) {
         matrix = new int[size][size];
     }
+
+    public void addNode(Node node) {
+
+    }
+    public void addEdge(int src, int dst) {
+
+    }
+    public boolean checkEdge(int src, int dst ) {
+
+    }
+    public void print() {
+
+    }
+
 }
 class Node {
     int data;
