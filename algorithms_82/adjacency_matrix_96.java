4e846001c847ad857feac9b45dc096fe20f56cd9
JuleeC <juleshummel44@gmail.com>
2025-03-02 21:50:51 +0100

    Just learning :)

--- algorithms_82/adjacency_matrix_96.java
+++ algorithms_82/adjacency_matrix_96.java
@@ -9,7 +9,7 @@ public class adjacency_matrix_96 {
     //space complexity: O(v*v)
 
     public static void main(String[] args) {
-
+        Graph graph = new Graph(5);
 
 
     }
@@ -26,10 +26,10 @@ class Graph {
 
     }
     public void addEdge(int src, int dst) {
-
+        matrix[src][dst] = 1;
     }
     public boolean checkEdge(int src, int dst ) {
-
+        if(matrix[src][dst] == 1) return true; else return false;
     }
     public void print() {
 
