9e542300df6a3183e444c4b792c7924619282b1b
JuleeC <juleshummel44@gmail.com>
2025-03-03 22:50:27 +0100

    Just learning :)

--- algorithms_82/adjancency_list_97.java
+++ algorithms_82/adjancency_list_97.java
@@ -1,4 +1,8 @@
 package algorithms_82;
 
 public class adjancency_list_97 {
+    // an array/arraylist of linkedlists.
+    // each LinkedList has a unique node at the head.
+    // all adjacent neighbors to that node are added to that nodes linkedlist
+
 }
