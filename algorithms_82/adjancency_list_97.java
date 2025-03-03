ae74ac38037e7466acb010c08f4e446d8ba368d6
JuleeC <juleshummel44@gmail.com>
2025-03-03 22:51:14 +0100

    Just learning :)

--- algorithms_82/adjancency_list_97.java
+++ algorithms_82/adjancency_list_97.java
@@ -5,4 +5,11 @@ public class adjancency_list_97 {
     // each LinkedList has a unique node at the head.
     // all adjacent neighbors to that node are added to that nodes linkedlist
 
+    // runtime complexity to check an Edge: O(v)
+    // space complexity: O(v + e)
+
+    public static void main(String[] args) {
+
+
+    }
 }
