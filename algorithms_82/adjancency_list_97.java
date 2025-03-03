43fa85dc54f0bad645c689c5aae0637d235b0964
JuleeC <juleshummel44@gmail.com>
2025-03-03 22:55:32 +0100

    Just learning :)

--- algorithms_82/adjancency_list_97.java
+++ algorithms_82/adjancency_list_97.java
@@ -15,6 +15,19 @@ public class adjancency_list_97 {
     public static void main(String[] args) {
         Graph_ graph = new Graph_();
 
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(4,0);
+        graph.addEdge(4,2);
+
     }
 }
 
