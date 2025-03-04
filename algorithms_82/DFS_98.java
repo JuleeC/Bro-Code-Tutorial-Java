08bc38b0c9cbc2055ba50d181b7e5550188016c7
JuleeC <juleshummel44@gmail.com>
2025-03-04 19:28:45 +0100

    just learning :)

--- algorithms_82/DFS_98.java
+++ algorithms_82/DFS_98.java
@@ -6,5 +6,10 @@ public class DFS_98 {
 
     // 1. pick a route
     // 2. Keep going until you reach a dead end, or a previously visited node
-    //
+    // 3. Backtrack to last node that has unvisited adjacent neighbors
+
+    public static void main(String[] args) {
+
+
+    }
 }
