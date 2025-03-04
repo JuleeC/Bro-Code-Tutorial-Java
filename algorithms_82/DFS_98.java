96d7bdcaa78732908d271d020717a196e159f3d7
JuleeC <juleshummel44@gmail.com>
2025-03-04 19:28:17 +0100

    just learning :)

---
+++ algorithms_82/DFS_98.java
@@ -0,0 +1,10 @@
+package algorithms_82;
+
+public class DFS_98 {
+    // a search algorithm for traversing
+    // a tree or graph data structure
+
+    // 1. pick a route
+    // 2. Keep going until you reach a dead end, or a previously visited node
+    //
+}
