cd7e6106831d28a0c43b7bc80d70075e97492c75
JuleeC <juleshummel44@gmail.com>
2025-03-05 21:24:39 +0100

    just learning :)

---
+++ algorithms_82/BFS_99.java
@@ -0,0 +1,73 @@
+package algorithms_82;
+
+import java.util.ArrayList;
+
+public class BFS_99 {
+    public static void main(String[] args) {
+        Graph___ graph = new Graph___(5);
+        graph.addNode(new Node('A'));
+        graph.addNode(new Node('B'));
+        graph.addNode(new Node('C'));
+        graph.addNode(new Node('D'));
+        graph.addNode(new Node('E'));
+
+        graph.addEdge(0,1);
+        graph.addEdge(1,2);
+        graph.addEdge(2,3);
+        graph.addEdge(2,4);
+        graph.addEdge(4,0);
+        graph.addEdge(4,2);
+
+        graph.print();
+
+
+    }
+}
+class Graph___ {
+    ArrayList<Node> nodes;
+    int[][] matrix;
+
+    Graph___(int size)  {
+        nodes = new ArrayList<>();
+        matrix = new int[size][size];
+
+    }
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+    public void addEdge(int src, int dst) {
+        matrix[src][dst] = 1;
+    }
+    public boolean checkEdge(int src, int dst ) {
+        if(matrix[src][dst] == 1) return true; else return false;
+    }
+
+
+
+    public void print() {
+        System.out.print("  ");
+        for(Node node : nodes) System.out.print( node.data + " ");
+
+        System.out.println();
+
+        for(int i = 0; i< matrix.length; i++) {
+            System.out.print(nodes.get(i).data + " ");
+            for(int j = 0; j< matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+
+
+}
+class Node___ {
+    char data;
+
+
+    Node___(char data) {
+        this.data = data;
+    }
+}
+
