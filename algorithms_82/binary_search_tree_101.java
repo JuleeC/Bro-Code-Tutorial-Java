0c4ea62b241a39a3a583b437112cb367448b3fef
JuleeC <juleshummel44@gmail.com>
2025-03-07 15:05:15 +0100

    Just learning :)

--- algorithms_82/binary_search_tree_101.java
+++ algorithms_82/binary_search_tree_101.java
@@ -1,4 +1,27 @@
 package algorithms_82;
 
 public class binary_search_tree_101 {
+    BinarySearchTree tree = new BinarySearchTree();
+}
+
+class Nodee {
+    int data;
+    Nodee left;
+    Nodee right;
+
+
+    public Nodee(int data) {
+        this.data = data;
+    }
+}
+
+class BinarySearchTree {
+ Nodee root;
+
+ public void insert(Nodee node) {
+ }
+ private Nodee insertHelper(Nodee root, Nodee node) {
+     return null;
+ }
+ publi
 }
