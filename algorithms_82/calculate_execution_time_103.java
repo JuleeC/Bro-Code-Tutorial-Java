b2cb8bf6714b380bbc241151f05428d22a3d87bf
JuleeC <juleshummel44@gmail.com>
2025-03-10 22:31:31 +0100

    Just learning :)

--- algorithms_82/calculate_execution_time_103.java
+++ algorithms_82/calculate_execution_time_103.java
@@ -1,9 +1,10 @@
 package algorithms_82;
 
 public class calculate_execution_time_103 {
-    long start = System.nanoTime();
+    public static void main(String[] args) {
+        long start = System.nanoTime();
     // programm
 
     long duration = (System.nanoTime()- start) / 1000000;
-    System.out.println(duration + "ns");
+    System.out.println(duration + "ns");}
 }
