04447f989739dececb7fe4fd9472f69a64a98a62
JuleeC <juleshummel44@gmail.com>
2025-04-09 12:58:47 +0200

    testing

--- school/mgin/wuerzl/blink2/blink/main/adc.c
+++ school/mgin/wuerzl/blink2/blink/main/adc.c
@@ -1,33 +1,33 @@
-
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "sdkconfig.h"
-
+#include "sdkconfig.h"                                  //Libraries
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
-
-double spg_value = 0;
-
-static esp_adc_cal_characteristics_t adc1_chars;
-
-//main Methode
-void app_main(void)
+ 
+double spg_value = 0;                                   //spg_value den Wert 0 zuweisen (mit Datentyp double)
+ 
+static esp_adc_cal_characteristics_t adc1_chars;        //ADC Variable
+ 
+ 
+void app_main(void)                                     //Methode
 {
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
-
-    adc1_config_width(ADC_WIDTH_BIT_DEFAULT);
-    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
-
-    while (1) 
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);       //Charakterisierung von ADC
+ 
+    adc1_config_width(ADC_WIDTH_BIT_DEFAULT);                       //Konfiguration der Bandbreite
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);     //Pin vom ADC
+ 
+    while (1)                                           //Endlosschleife
     {
-        int adc_value = adc1_get_raw(ADC1_CHANNEL_4);
-        spg_value = (double)(adc_value)/4095*3.3;
-        printf("SPG Value: %f", spg_value);
-        printf("ADC Value: %d", adc_value);
-        printf("\n");
-        vTaskDelay(500/ portTICK_PERIOD_MS);
+        int adc_value = adc1_get_raw(ADC1_CHANNEL_4);   //adc_value nimmt den ADC Wert an
+        spg_value = (double) (adc_value) / 4095 * 3.3;  //Umrechnung von ADC Wert in die Spannung mit Formel
+        printf("ADC Value: %d", adc_value);             //Ausgabe von ADC Value
+        printf("\n");                                   //Zeilenumbruch
+ 
+        printf("Spannungsvalue: %f", spg_value);        //Ausgabe von spg_value
+        printf("\n");                                   //Zeilenumbruch
+        vTaskDelay(500/ portTICK_PERIOD_MS);            //Verzögerung
     }
 }
