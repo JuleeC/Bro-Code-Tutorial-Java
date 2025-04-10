89cbb0a7c27086733d3dff0ad42b304b942e1097
JuleeC <juleshummel44@gmail.com>
2025-04-10 17:08:04 +0200

    mouse over

--- flutter/mouse_over/lib/main.dart
+++ flutter/mouse_over/lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //to disable the debug banner
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -28,7 +30,9 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        //to set the color scheme of the app
+        // TRY THIS: Try changing the seedColor in the colorScheme below to
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple, brightness: Brightness.dark,),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
