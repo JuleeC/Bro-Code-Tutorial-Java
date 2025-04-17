58006b12c357217ab1561194c60cfba25d1349fc
JuleeC <juleshummel44@gmail.com>
2025-04-17 18:22:21 +0200

    skeleton of the program

--- flutter/learning/lib/main.dart
+++ flutter/learning/lib/main.dart
@@ -5,14 +5,25 @@ void main() {
 }
 
 //stateless widget
-//material app
-//scaffold
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //material app
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.dark,)
+          ),
+       
+      //scaffold
+      home: Scaffold(),
+    );
   }
 }
+
+
+
