b6d42fef29f7fe015d615528405f4812aac5a3a1
JuleeC <juleshummel44@gmail.com>
2025-04-19 10:18:38 +0200

    a

--- flutter/learning/lib/main.dart
+++ flutter/learning/lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(title!),
           centerTitle: true,
         ),
-        //if statement but dynamicc
+        //if statement but dynamic
         body: currentIndex == 0
             ? Center(
                 child: Text("Home"),
