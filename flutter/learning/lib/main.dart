708a2b5552a1b9c010b96c7d238cd46b15144b07
JuleeC <juleshummel44@gmail.com>
2025-04-21 18:01:02 +0200

    t

--- flutter/learning/lib/main.dart
+++ flutter/learning/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
       valueListenable: isDarkModeNotifier,
       builder: (context, value, child) {
         return MaterialApp(
+          title: title!,
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(
