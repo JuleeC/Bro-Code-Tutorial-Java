f29d4e0df77650026ea8fc47cd3cc77a6cc453b1
JuleeC <juleshummel44@gmail.com>
2025-04-21 17:46:53 +0200

    check box

--- flutter/learning/lib/views/pages/profile_page.dart
+++ flutter/learning/lib/views/pages/profile_page.dart
@@ -15,6 +15,7 @@ class _ProfilePageState extends State<ProfilePage> {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           TextField(
             decoration: InputDecoration(border: OutlineInputBorder()),
