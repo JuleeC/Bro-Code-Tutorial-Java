8580337ac99bdf04e2c0beb9d228823498482ee2
JuleeC <juleshummel44@gmail.com>
2025-04-27 15:14:08 +0200

    feat: update WelcomePage layout and add login button functionality

--- flutter/learning/lib/views/pages/welcome_page.dart
+++ flutter/learning/lib/views/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning/views/widget_tree.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -6,14 +7,28 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: Image.asset("assets/images/aes.png"),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0),
+              child: Image.asset("assets/images/aes.jpg"),
+            ),
+            FilledButton(onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return WidgetTree();
+                  },
+                ),
+              );
+              
+            }, child: Text("Login")),
+          ],
+        ),
       ),
     );
   }
