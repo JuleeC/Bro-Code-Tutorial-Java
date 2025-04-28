410d00f7d289872b3e49d1d9583f8cbd6a729056
JuleeC <juleshummel44@gmail.com>
2025-04-28 19:21:53 +0200

    snackbar

--- flutter/learning/lib/widgets/hero_widget.dart
+++ flutter/learning/lib/widgets/hero_widget.dart
@@ -9,7 +9,7 @@ class HeroWidget extends StatelessWidget {
       tag: "hero1",
       child: ClipRRect(
         borderRadius: BorderRadius.circular(20.0),
-        child: Image.asset("assets/images/aes.jpg"),
+        child: Image.asset("assets/images/aes.jpg", color: Colors.teal,colorBlendMode: BlendMode.darken,),
       ),
     );
   }
