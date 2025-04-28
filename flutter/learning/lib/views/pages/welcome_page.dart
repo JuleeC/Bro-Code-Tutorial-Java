af67b6ec21f90fcf2cc9b3d8f8c30023a2547854
JuleeC <juleshummel44@gmail.com>
2025-04-28 19:52:24 +0200

    lottie

--- flutter/learning/lib/views/pages/welcome_page.dart
+++ flutter/learning/lib/views/pages/welcome_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learning/views/widget_tree.dart';
-import 'package:learning/widgets/hero_widget.dart';
+
+import 'package:lottie/lottie.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -13,7 +14,11 @@ class WelcomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            HeroWidget(),
+            Lottie.asset(
+              "assets/lotties/welcome.json",
+              width: 200,
+              height: 200,
+            ),
             FilledButton(
               onPressed: () {
                 Navigator.push(
