774029bd2a273243d2bc3342184d67ca2ae6f516
JuleeC <juleshummel44@gmail.com>
2025-05-02 13:20:05 +0200

    testing

--- flutter/learning/lib/views/pages/welcome_page.dart
+++ flutter/learning/lib/views/pages/welcome_page.dart
@@ -10,60 +10,76 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Lottie.asset(
-              "assets/lotties/welcome.json",
-              width: 200,
-              height: 200,
-            ),
-            FittedBox(
-              child: Text(
-                "Flutter App",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50.0,
-                  letterSpacing: 50.0,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Lottie.asset(
+                  "assets/lotties/welcome.json",
+                  width: 200,
+                  height: 200,
+                ),Lottie.asset(
+                  "assets/lotties/welcome.json",
+                  width: 200,
+                  height: 200,
+                ),Lottie.asset(
+                  "assets/lotties/welcome.json",
+                  width: 200,
+                  height: 200,
+                ),Lottie.asset(
+                  "assets/lotties/welcome.json",
+                  width: 200,
+                  height: 200,
                 ),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                FittedBox(
+                  child: Text(
+                    "Flutter App",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50.0,
+                      letterSpacing: 50.0,
+                    ),
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text("Get Started"),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                ),
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage();
+                        },
+                      ),
+                    );
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
                   ),
-                );
-              }, style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text("Login"),
+                  child: Text("Get Started"),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage();
+                        },
+                      ),
+                    );
+                  }, style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text("Login"),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
