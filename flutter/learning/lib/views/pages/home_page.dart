e4b84e3bde785a45319a41299a6f2a3a4c4d1421
JuleeC <juleshummel44@gmail.com>
2025-05-04 19:18:06 +0200

    refactor: improve widget generation in HomePage for better readability

--- flutter/learning/lib/views/pages/home_page.dart
+++ flutter/learning/lib/views/pages/home_page.dart
@@ -13,6 +13,16 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             HeroWidget(title: "Flutter App"),
+            // you can use this to create a list of widgets
+            // if you want to see how it looks like with the SingleChildScrollView
+            Column(children: List.generate(5, (index) {
+              return ContainerWidget(
+                title: "Basic Layout",
+                description: "This is a basic layout of a flutter app.",
+              );
+            
+            },),
+            ),
             ContainerWidget(
               title: "Basic Layout",
               description: "This is a basic layout of a flutter app.",
