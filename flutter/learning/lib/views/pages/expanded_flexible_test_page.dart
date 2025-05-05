2e153a14b7a78cdb556129b24196229e42671b92
JuleeC <juleshummel44@gmail.com>
2025-05-05 13:55:06 +0200

    refactor: update layout structure in ExpandedFlexibleTestPage for improved readability

--- flutter/learning/lib/views/pages/expanded_flexible_test_page.dart
+++ flutter/learning/lib/views/pages/expanded_flexible_test_page.dart
@@ -7,49 +7,52 @@ class ExpandedFlexibleTestPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          // Expanded(flex: 2,child: Container(color: Colors.teal)),
-          // Expanded(child: Container(color: Colors.teal)),
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.teal,
-                  height: 20,
-                  child: Text("Hello"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Expanded(flex: 2,child: Container(color: Colors.teal)),
+            // Expanded(child: Container(color: Colors.teal)),
+            
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.teal,
+                    height: 20,
+                    child: Text("Hello"),
+                  ),
                 ),
-              ),
-              Flexible(
-                child: Container(
-                  color: Colors.orange,
-                  height: 20,
-                  child: Text("Hello"),
+                Flexible(
+                  child: Container(
+                    color: Colors.orange,
+                    height: 20,
+                    child: Text("Hello"),
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Divider(),
-          Row(
-            children: [
-              Flexible(
-                flex: 4,
-                child: Container(
-                  color: Colors.teal,
-                  height: 20,
-                  child: Text("Hello"),
+              ],
+            ),
+            Divider(),
+            Row(
+              children: [
+                Flexible(
+                  flex: 4,
+                  child: Container(
+                    color: Colors.teal,
+                    height: 20,
+                    child: Text("Hello"),
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.orange,
-                  height: 20,
-                  child: Text("Hello"),
+                Expanded(
+                  child: Container(
+                    color: Colors.orange,
+                    height: 20,
+                    child: Text("Hello"),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
