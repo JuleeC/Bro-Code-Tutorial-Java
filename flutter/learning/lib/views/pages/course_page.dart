e568ba586f2eb7f58798bd6457d1d4b751c9e926
JuleeC <juleshummel44@gmail.com>
2025-05-06 19:28:54 +0200

    refactor: remove duplicate super.initState() call in CoursePage

--- flutter/learning/lib/views/pages/course_page.dart
+++ flutter/learning/lib/views/pages/course_page.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:learning/widgets/hero_widget.dart';
 
-class CoursePage extends StatelessWidget {
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
 
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  
+  @override
+  void initState() {
+    super.initState();
+    getData();
+    super.initState();
+    // Initialize any data or state here if needed
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
