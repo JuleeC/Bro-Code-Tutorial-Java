adc8bf5ec0cb5b92fb34563736f4029e6c5874f7
JuleeC <juleshummel44@gmail.com>
2025-05-06 19:37:17 +0200

    fixing

--- flutter/learning/lib/views/pages/course_page.dart
+++ flutter/learning/lib/views/pages/course_page.dart
@@ -25,7 +25,7 @@ class _CoursePageState extends State<CoursePage> {
     if (response.statusCode == 200) {
       var jsonResponse =
           convert.jsonDecode(response.body) as Map<String, dynamic>;
-      var itemCount = jsonResponse['totalItems'];
+      var itemCount = jsonResponse['activity'];
       print('Number of books about http: $itemCount.');
     } else {
       print('Request failed with status: ${response.statusCode}.');
