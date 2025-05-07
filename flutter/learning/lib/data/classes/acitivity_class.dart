f88e49c37dc57c3eb197d967171d9b0074803483
JuleeC <juleshummel44@gmail.com>
2025-05-07 18:48:19 +0200

    feat: add Activity class with relevant properties

---
+++ flutter/learning/lib/data/classes/acitivity_class.dart
@@ -0,0 +1,20 @@
+class Activity {
+  final String activity;
+  final String type;
+  final String availability;
+  final int participants;
+  final String price;
+  final String accessibility;
+  final String duration;
+  final String kidFriendly;
+  final String link;
+  final String key;
+
+  
+
+    
+     
+    
+  }
+
+}
